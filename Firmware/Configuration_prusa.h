59535e0824a1f312eab12aff39b811439e692692
Clancy <clancydgaroutte@gmail.com>
2021-12-02 16:55:41 -0700

    INCREASED E-AXIS CURRENT TO 1.5A

--- Firmware/Configuration_prusa.h
+++ Firmware/Configuration_prusa.h
@@ -283,10 +283,10 @@
 #define TMC2130_SG_THRS_HOME {3, 3, TMC2130_SG_THRS_Z, TMC2130_SG_THRS_E}
 
 //new settings is possible for vsense = 1, running current value > 31 set vsense to zero and shift both currents by 1 bit right (Z axis only)
-#define TMC2130_CURRENTS_H {16, 20, 35, 30}  // default holding currents for all axes
+#define TMC2130_CURRENTS_H {16, 20, 35, 95} // {16,20,35,30} // default holding currents for all axes
 #define TMC2130_CURRENTS_FARM 36             // E 805 mA peak for ECool/farm mode
-#define TMC2130_CURRENTS_R {16, 20, 35, 30}  // default running currents for all axes
-#define TMC2130_CURRENTS_R_HOME {8, 10, 20, 18}  // homing running currents for all axes
+#define TMC2130_CURRENTS_R {16, 20, 35, 95}  // {16,20,35,30}// default running currents for all axes
+#define TMC2130_CURRENTS_R_HOME {8, 10, 20, 95}  // {8,10,20,18}// homing running currents for all axes
 
 #define TMC2130_STEALTH_Z
 #define TMC2130_DEDGE_STEPPING
@@ -423,7 +423,7 @@
  *------------------------------------*/
 
 // Motor Current settings for Einsy/tmc = 0..63
-#define MOTOR_CURRENT_PWM_RANGE 63
+#define MOTOR_CURRENT_PWM_RANGE 95
 
 /*------------------------------------
  BED SETTINGS
